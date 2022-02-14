9e728677e1fbb30b69419bf5002c9431f542facb
jisanlamichhane1 <79670058+jisanlamichhane1@users.noreply.github.com>
2022-02-14 17:18:21 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function clearCanvas(){
+    background("white");
+}
+function setup(){
+    canvas = creatCanvas(280,280);
+    canvas.center();
+    background("white");
+
+}
+function clearCanvas(){
+    background("white");
+}
